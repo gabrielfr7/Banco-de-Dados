43087425fe69e2f7cc985579b7552c46607ed90a
Gabriel Freire <freire.anjos08@aluno.ifce.edu.br>
2025-08-21 13:57:46 -0300

    Add files via upload

---
+++ Ex01/excriacaotabela01.sql
@@ -0,0 +1,32 @@
+-- create database dbAula030625;
+
+-- use dbAula030625;
+
+-- create table TbAluno(
+
+-- codAluno int not null,
+-- matriculaAluno varchar(20) not null , 
+-- nomeAluno varchar(225) not null, 
+-- idadeAluno int,
+-- sexoAluno char(1), 
+-- dataNascimentoAluno date not null,
+-- telefoneAluno varchar(15),
+-- emailAluno varchar(225),
+-- ruaAluno varchar(225),
+-- bairroAluno varchar (100),
+-- numeroAluno int(4),
+-- complementoAluno varchar(100),
+-- estadoAluno char(2),
+-- cidadeAluno varchar (100) default 'Maranguape',
+-- dataInsercaoAluno datetime not null default current_timestamp,
+-- constraint chkIdadeAluno check (idadeAluno >= 26 and idadeAluno <= 20),
+-- constraint unqMatricula unique (matriculaAluno),
+-- constraint unqCod unique (codAluno)
+-- )
+
+
+-- not null e default SÃO NA LINHA
+-- unique e check SÃO NO FINAL DA LINHA
+
+
+
